1acd3c696e133381bdff621d6eb9abaa77daf045
f-oyama <f-oyama>
2020-05-24 21:30:03 +0900

    不要なアクセス修飾子を削除

--- src/main/java/com/example/learning/slacklinkage/appointment/infrastructure/repository/AppointmentRepository.java
+++ src/main/java/com/example/learning/slacklinkage/appointment/infrastructure/repository/AppointmentRepository.java
@@ -6,7 +6,7 @@ import com.example.learning.slacklinkage.appointment.domain.model.Appointment;
 import java.util.List;
 
 public interface AppointmentRepository {
-    public Appointment save(Appointment appointment);
-    public List<Appointment> selectAll();
-    public boolean deleteAll();
+    Appointment save(Appointment appointment);
+    List<Appointment> selectAll();
+    boolean deleteAll();
 }
